da6e4cdca566c342440395cb864deea081e37835
Praneetha7781 <88105267+Praneetha7781@users.noreply.github.com>
2021-07-29 03:32:08 +0530

    Add files via upload

---
+++ Bos.js
@@ -0,0 +1,30 @@
+﻿function myfunction() {
+
+    var tableElement = document.getElementsByTagName("table")[0];
+    var newrow = tableElement.insertRow(1);
+    var cell1 = newrow.insertCell(0);
+    var cell2 = newrow.insertCell(1);
+    var cell3 = newrow.insertCell(2);
+    var name = document.getElementById("ProductEntry").value;
+    document.getElementById("ProductEntry").value = "";
+    var quantity;
+    var edate;
+    if (name == "1112105622") {
+        name = "Bread";
+        quantity = "210";
+        edate = "5 June 2022";
+    }
+    if (name == "2425509923") {
+        name = "Maggi";
+        quantity = "550";
+        edate = "9 Sept 2023";
+    }
+    if (name == "1231007722") {
+        name = "Moov";
+        quantity = "100";
+        edate = "7 July 2022";
+    }
+    cell1.innerHTML = name;
+    cell2.innerHTML = quantity;
+    cell3.innerHTML = edate;
+}
---
+++ Hackathon.js
@@ -0,0 +1,74 @@
+﻿var db = openDatabase("mydb", "1.0", "Store", 2 * 1024 * 1024);
+
+function myfunction() {
+    var tableElement = document.getElementsByTagName("table")[0];
+    var newrow = tableElement.insertRow(1);
+    var cell1 = newrow.insertCell(0);
+    var cell2 = newrow.insertCell(1);
+    var name = document.getElementById("ProductEntry").value;
+    var quantity = document.getElementById("QuantityEntry").value;
+    if (name == "111")
+        name = "Bread";
+    if (name == "242")
+        name = "Tooth Brush";
+    if (name == "1542")
+        name = "Bed Sheet";
+
+    cell1.innerHTML = name;
+    cell2.innerHTML = quantity;
+}
+
+function clearfunction() {
+    var table = document.getElementById("table");
+    var x = table.rows.length;
+    var Quan;
+
+    if (table.rows.length == 1) {
+        alert("No rows");
+    }
+    else {
+        for (i = 0; i < x - 1; i++) {
+            table.deleteRow(1);
+        }
+        alert("Transaction Successful");
+        db.transaction(function (tx) {
+            tx.executeSql('CREATE TABLE IF NOT EXISTS Shelf (Id, Quantity)');
+            tx.executeSql('INSERT INTO Shelf (Id, Quantity) VALUES (1, 6)');
+            tx.executeSql('INSERT INTO Shelf (Id, Quantity) VALUES (2, 50)');
+        })
+        
+        db.transaction(function (tx) {
+            tx.executeSql(
+                'SELECT * FROM Shelf where Id=1',
+                [],
+                function (tx, results) {
+                    Quan = results.rows.item(0).Quantity;
+                    Quan=Quan - 2;
+                    if (Quan<5)
+                       alert("Hi Mark, There are only " + Quan + " Bread packets on Shelf. Please Refill") });
+
+
+        });
+    }
+
+}
+
+function Supervisor() {
+    db.transaction(function (tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS Product (Id, ProductDescription, BatchNum, ExpiryDate)');
+        tx.executeSql('INSERT INTO Product (Id, ProductDescription, BatchNum, ExpiryDate) VALUES (1, "Bread", "1234", "08-08-2021")');
+        tx.executeSql('INSERT INTO Product (Id, ProductDescription, BatchNum, ExpiryDate) VALUES (2, "Butter", "1235", "07-07-2021")');
+    })
+   
+
+    db.transaction(function (tx) {
+        tx.executeSql(
+            'SELECT * FROM Product where ExpiryDate="07-07-2021"',
+         [],
+            function (tx, results) {
+                alert("Hi Mark, There are 12 " + results.rows.item(0).ProductDescription + " packets in lot " + results.rows.item(0).BatchNum+"  which will expire in 3 days. Here are few recommendations to sell them as soon as possible: Buy one Butter and Get a bread packet free or Buy a 1+1 Bread Packet.  ");
+            });
+
+
+    });
+}
